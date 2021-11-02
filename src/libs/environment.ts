d804bc73cd30b9df8ce223d586f4c2a78fb1ed21
Emily Dong <emilyhfdong@gmail.com>
2021-11-02 02:02:06 -0400

    remove unused env vars

--- src/libs/environment.ts
+++ src/libs/environment.ts
@@ -12,16 +12,6 @@ export const config = {
     .required()
     .asString(),
   localStorageJsonKey: env.get("LOCAL_STORAGE_JSON_KEY").required().asString(),
-  libby: {
-    toronto: {
-      cardNumber: env.get("TORONTO_CARD_NUMBER").required().asString(),
-      pin: env.get("TORONTO_PIN").required().asString(),
-    },
-    calgary: {
-      cardNumber: env.get("CALGARY_CARD_NUMBER").required().asString(),
-      pin: env.get("CALGARY_PIN").required().asString(),
-    },
-  },
   overdriveApiKey: env.get("OVERDRIVE_API_KEY").required().asString(),
   stateMachineArn: env.get("STATE_MACHINE_ARN").asString(),
 }
--- src/libs/notion.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from "@notionhq/client"
-import { config } from "./environment"
-
-export const notion = new Client({
-  auth: config.notion.token,
-})
